f50034e74d4a2fd7063f50151e5f61679ccb7e42
smasish <asish_cse@yahoo.com>
2019-07-02 23:16:58 -0500

    Scanning Progressbar added

--- app/src/main/java/cacs/ul/privacydemo/PermissionScannerActivity.java
+++ app/src/main/java/cacs/ul/privacydemo/PermissionScannerActivity.java
@@ -9,6 +9,16 @@ public class PermissionScannerActivity extends AppCompatActivity {
 
     private Context con;
 
+    public String[] text_info = {"android.permission.SEND_SMS","android.permission.CALL_PHONE"};
+    public String[] personal_info = {"android.permission.READ_CALENDAR", "android.permission.READ_CALL_LOG", "android.permission.READ_CONTACTS",
+            "android.permission.READ_PROFILE", "android.permission.READ_SMS", "android.permission.READ_SOCIAL_STREAM"};
+    public String[] location_info = {"android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"};
+
+    public String[] can_impact_battery = {"android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION", "android.permission.BLUETOOTH",
+            "android.permission.CALL_PHONE", "android.permission.FLASHLIGHT", "android.permission.NFC"};
+    public String[] can_change_system = {"android.permission.WRITE_SETTINGS"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
